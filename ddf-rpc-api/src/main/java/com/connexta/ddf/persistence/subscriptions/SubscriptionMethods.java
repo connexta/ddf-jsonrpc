15c1c37262f6cc9509e1e80c1969e186e27a6e36
Ryan Zwiefelhofer <ryan.zwiefelhofer@connexta.com>
2020-10-12 14:46:40 -0700

    Cleaned up miscellaneous comments

--- ddf-rpc-api/src/main/java/com/connexta/ddf/persistence/subscriptions/SubscriptionMethods.java
+++ ddf-rpc-api/src/main/java/com/connexta/ddf/persistence/subscriptions/SubscriptionMethods.java
@@ -97,7 +97,7 @@ public class SubscriptionMethods implements MethodSet {
         ids.stream()
             .collect(
                 Collectors.toMap(Function.identity(), subscriptions::contains, (l, r) -> l || r));
-    return MapFactory.mapOf("isSubscribed", resultMap); // TODO (RCZ) - in progress
+    return MapFactory.mapOf("isSubscribed", resultMap); 
   }
 
   private List<Map<String, Object>> query(String email) throws PersistenceException {
